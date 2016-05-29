b047b5c723bf2ba2c6d17a7168ff67d8ffc92eaf
user <user@localhost>
2016-05-29 12:33:37 +0000

    First Commit

---
+++ main.c
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: user
+ *
+ * Created on May 29, 2016, 12:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int size, char** arguments) {
+    
+    if( falscheparameteranzahl
+            ) {
+	      printf("Fehler bei malloc....\n");
+	      return EXIT_FAILURE;
+	   }
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=prak5_simlecopy
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/prak5_simlecopy
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=prak5simlecopy.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/prak5simlecopy.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=prak5_simlecopy
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/prak5_simlecopy
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=prak5simlecopy.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/prak5simlecopy.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
