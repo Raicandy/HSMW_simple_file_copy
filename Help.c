f5a4eba7986797634b870422befba1c781629c4b
user <user@localhost>
2016-05-29 13:33:18 +0000

    Update 3

--- Help.c
+++ Help.c
@@ -1,67 +1,80 @@
+/* Kopierprogramm */
+/* ist Zieldatei bereits vorhanden, wird abgefragt, ob sie überschrieben 
+   werden soll
+*/
+
+// gcc -Wall -g -o name name.c
+
+#define	BLKSIZE		4096
+#define _LARGEFILE_SOURCE
+#define _FILE_OFFSET_BITS 64
 #include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
 #include <assert.h>
-#define	BLKSIZE		4096
 
-
-int main(int argc, char * argv[]) {
-
+int main (int argc, char ** argv)
+{
 	char * ptr;
-	int bytes;
+	int fdin, fdout, bytes;
+	long long counter = 0;
+	char ch;	
 	struct stat status;
 	mode_t mode;
-		
-	if (argc != 3){
-        	fprintf(stderr, "Es müssen 3 Argumente übergeben müssen: %s Quelle Ziel \n" , argv [0]);
-        	return EXIT_FAILURE;
+	 
+	// Check # args	
+	if (argc != 3) {
+		fprintf(stderr, "   usage: copy source dest \n");
+		exit(EXIT_FAILURE);
 	}
-    
-        
-        int fdin = open(argv[1], O_RDONLY);
-	if(fdin < 0){
-		fprintf(stderr, "Kann datei nich öffnen: %s \n", argv[1]);
-		return EXIT_FAILURE;
+	
+	// open source	 
+	if ((fdin = open (argv[1], O_RDONLY)) == -1) {
+		fprintf(stderr, "   Error! Wrong file: %s Reason: %s \n", 
+                    argv[1], strerror(errno));
+		exit(EXIT_FAILURE);
 	}
-
-		// read access modes
+	
+	// read access modes
 	if(fstat(fdin, &status) < 0) {
 		perror("fstat: ");
 	        exit(EXIT_FAILURE);
 	}
 	
-	mode = status.st_mode & ~S_IFMT;
-
+	mode = status.st_mode & ~S_IFMT;                                        
 	
-	int fdout = open(argv[2], O_WRONLY| O_CREAT| O_EXCL, 0);
-	if(fdout < 0 && errno == EEXIST){
-		fprintf(stderr, "Datei %s exestiert bereits, überschreiben(y/n)?", argv[2]);
-		char ch;
-		if((ch = getchar()) == 'y') {
-			if ((fdout = open(argv[2], O_WRONLY | O_TRUNC)) == -1) {
-				fprintf(stderr, "Fehler, falsche Datei: %s Grund: %s \n", argv[2], strerror(errno));
-				exit(EXIT_FAILURE);
-			}
+	// create destination  
+    if ((fdout = open (argv[2], O_CREAT | O_WRONLY | O_EXCL, 0)) < 0) {
+       	if (errno == EEXIST) {
+    		fprintf(stderr, "Destination %s already exists, overwrite (y/n)?", 
+                    argv[2]);
+    		if ((ch = getchar()) == 'y') {
+    			if ((fdout = open (argv[2], O_WRONLY | O_TRUNC)) == -1) {
+    				fprintf(stderr, "   Error! Wrong file: %s Reason: %s \n", 
+                        argv[2], strerror(errno));
+					exit(EXIT_FAILURE);
+				}  
+			}  else { 
+				exit(EXIT_SUCCESS);
+			}	
 		} else {
+			fprintf(stderr, "   Error! Wrong file: %s Reason: %s \n", 
+                argv[2], strerror(errno));
 			exit(EXIT_FAILURE);
 		}
-	} else {
-		fprintf(stderr, "Fehler, falsche Datei: %s Grund: %s \n", argv[2], strerror(errno));
-		exit(EXIT_FAILURE);
 	}
-
-
-
+	                                                                                
+	
 	ptr = (char *)malloc(BLKSIZE);
 	if(ptr == NULL) {
 	        perror("malloc: ");
 	        exit(EXIT_FAILURE);
-   	}
+    }
         
 	  
 	do {
@@ -69,7 +82,8 @@ int main(int argc, char * argv[]) {
 	    if(bytes < 0) {
 	      perror("read: ");
 	      exit(EXIT_FAILURE);
-        }
+		}
+	counter += bytes;
 	    bytes = write( fdout, ptr, bytes);
 	    
         if(bytes < 0) {
@@ -79,13 +93,13 @@ int main(int argc, char * argv[]) {
 
 	} while (bytes == BLKSIZE);
 	
+	printf("%lld", counter);
+
 	if(fchmod(fdout, mode) < 0) 
 		perror("cannot fchmod destination:");
 	
 	free(ptr);
 	assert(close(fdin)>=0);
 	assert(close(fdout)>=0);
-
-    return (EXIT_SUCCESS);
+	return EXIT_SUCCESS;   			
 }
-
